fd66bdf3529876631d05c34f0f2efcdafd53ed8d
LunEyx <luneyxlab@gmail.com>
2024-03-21 03:58:58 +0800

    Categorize record words by letter count #2

--- src/components/recordRegion/WordRecordCard.tsx
+++ src/components/recordRegion/WordRecordCard.tsx
@@ -5,23 +5,61 @@ import {
   Tag,
   TagLabel,
   TagCloseButton,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  Box,
+  AccordionPanel,
 } from '@chakra-ui/react'
 import { useRecordContext } from '../../contexts/RecordContext'
 
 const RecordCard = () => {
   const { words, removeRecord } = useRecordContext()
 
+  const wordsByLength: { [key: number]: string[] } = {}
+
+  for (const word of words) {
+    const length = word.length
+    if (wordsByLength[length]) {
+      wordsByLength[length].push(word)
+    } else {
+      wordsByLength[length] = [word]
+    }
+  }
+
   return (
     <Card>
       <CardBody>
-        <Flex gap={2} wrap="wrap">
-          {Array.from(words).map((word) => (
-            <Tag key={word}>
-              <TagLabel>{word}</TagLabel>
-              <TagCloseButton onClick={() => removeRecord(word)} />
-            </Tag>
+        <Accordion
+          allowMultiple
+          defaultIndex={Array.from(
+            Array(Object.keys(wordsByLength).length).keys(),
+          )}
+        >
+          {Object.entries(wordsByLength).map(([length, words]) => (
+            <AccordionItem key={length}>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    {length} letter{parseInt(length) > 1 && 's'}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel>
+                <Flex gap={2} wrap="wrap">
+                  {words.map((word) => (
+                    <Tag key={word}>
+                      <TagLabel>{word}</TagLabel>
+                      <TagCloseButton onClick={() => removeRecord(word)} />
+                    </Tag>
+                  ))}
+                </Flex>
+              </AccordionPanel>
+            </AccordionItem>
           ))}
-        </Flex>
+        </Accordion>
       </CardBody>
     </Card>
   )
