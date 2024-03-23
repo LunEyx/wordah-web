582f189771b7ec721d6b8233258b1f43193c353f
LunEyx <luneyxlab@gmail.com>
2024-03-24 04:35:26 +0800

    Add woah image

--- src/components/character/CharacterContainer.tsx
+++ src/components/character/CharacterContainer.tsx
@@ -1,7 +1,12 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Image } from '@chakra-ui/react'
+import WoahImg from '../../assets/woah.jpg'
 
 const CharacterContainer = () => {
-  return <Box w="300px" h="200px" backgroundColor="lightblue"></Box>
+  return (
+    <Box w="300px" h="200px" backgroundColor="lightblue">
+      <Image src={WoahImg} />
+    </Box>
+  )
 }
 
 export default CharacterContainer
