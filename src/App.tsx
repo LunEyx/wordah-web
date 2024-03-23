549f5c77244e30f647d7a1a661f1f5db2ec7a989
LunEyx <luneyxlab@gmail.com>
2024-03-24 02:44:08 +0800

    Update menu and status card

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import RecentWordCard from './components/recentRegion/RecentWordCard'
 import RecordCard from './components/recordRegion/WordRecordCard'
 import StatusCard from './components/statusRegion/StatusCard'
 import { useRecordContext } from './contexts/RecordContext'
-import DrawerMenu from './components/menu/DrawerMenu'
 import AchievementModal from './components/modals/AchievementModal'
+import SideMenu from './components/menu/SideMenu'
 
 function App() {
   const { isLoading } = useRecordContext()
   return (
     !isLoading && (
       <>
+        <Box position="absolute" top="0" right="0" p={4}>
+          <StatusCard />
+        </Box>
         <Container maxW="container.lg" mt={16}>
           <Flex flexDir="column" justify="center" rowGap={4} w="100%">
-            <StatusCard />
             <RecentWordCard />
             <InputCard />
             <RecordCard />
@@ -24,8 +26,8 @@ function App() {
           </Box>
         </Container>
 
-        <Box position="absolute" top="0" right="0" p={4}>
-          <DrawerMenu />
+        <Box position="absolute" top="0" left="0" p={4}>
+          <SideMenu />
         </Box>
 
         <AchievementModal />
---
+++ src/components/menu/ChallengeButton.tsx
@@ -0,0 +1,8 @@
+import { IconButton } from '@chakra-ui/react'
+import { QuestionOutlineIcon } from '@chakra-ui/icons'
+
+const ChallengeButton = () => {
+  return <IconButton aria-label="menu" icon={<QuestionOutlineIcon />} />
+}
+
+export default ChallengeButton
