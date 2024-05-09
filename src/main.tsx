2b2beacd653837e29b11f9fb34ec4ef88587dd44
LunEyx <luneyxlab@gmail.com>
2024-05-10 03:02:29 +0800

    Setup redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import RecordProvider from './contexts/RecordContext'
 import './index.css'
 import { AchievementProvider } from './contexts/AchievementContext.tsx'
 import { ModalProvider } from './contexts/ModalContext.tsx'
+import { store } from './app/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <RecordProvider>
-        <ModalProvider>
-          <AchievementProvider>
-            <App />
-          </AchievementProvider>
-        </ModalProvider>
-      </RecordProvider>
+      <Provider store={store}>
+        <RecordProvider>
+          <ModalProvider>
+            <AchievementProvider>
+              <App />
+            </AchievementProvider>
+          </ModalProvider>
+        </RecordProvider>
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>,
 )
