a00aed2fa1760557d07cf962c581477fcebd7588
LunEyx <luneyxlab@gmail.com>
2024-05-10 21:46:24 +0800

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import { useContext } from 'react'
-import { Box, Container, Flex } from '@chakra-ui/react'
-import InputCard from './components/inputRegion/InputCard'
-import RecentWordCard from './components/recentRegion/RecentWordCard'
-import StatusCard from './components/statusRegion/StatusCard'
-import { RecordContext } from './contexts/RecordContext'
-import AchievementModal from './components/modals/AchievementModal'
-import SideMenu from './components/menu/SideMenu'
-import CharacterContainer from './components/character/CharacterContainer'
-import DialogueContainer from './components/character/DialogueContainer'
-import { DialogueProvider } from './contexts/DialogueContext'
-import WordRecordModal from './components/recordRegion/WordRecordModal'
-import ProgressContainer from './components/main/ProgressContainer'
 import { useAppSelector } from './hooks/redux'
 import InitializePage from './pages/InitializePage'
 import MainPage from './pages/MainPage'
 
 function App() {
-  // const { isLoading } = useContext(RecordContext)
   const isInitialized = useAppSelector((state) => state.game.isInitialized)
 
   return isInitialized ? <MainPage /> : <InitializePage />
