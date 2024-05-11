37141c3cf266613d57a679b84e68530eb0849e72
LunEyx <luneyxlab@gmail.com>
2024-05-11 23:30:29 +0800

    Update images

--- src/components/menu/Portrait.tsx
+++ src/components/menu/Portrait.tsx
@@ -1,15 +1,17 @@
 import { Box, Image } from '@chakra-ui/react'
-import { useAppSelector } from '../../hooks/redux'
+import { useAppDispatch, useAppSelector } from '../../hooks/redux'
 import { portraitBorderImages, portraitImages } from '../../constants/portrait'
+import { toggleModal } from '../../features/modal/settingsSlice'
 
 const Portrait = () => {
+  const dispatch = useAppDispatch()
   const portraitBorderIndex = useAppSelector(
     (state) => state.user.portraitBorderIndex,
   )
   const portraitIndex = useAppSelector((state) => state.user.portraitIndex)
 
   return (
-    <Box pos="relative">
+    <Box pos="relative" onClick={() => dispatch(toggleModal())}>
       <Image src={portraitImages[portraitIndex]} />
       <Image
         pos="absolute"
--- src/constants/portrait.ts
+++ src/constants/portrait.ts
@@ -1,12 +1,17 @@
 import PortraitBorder00 from '../assets/portrait/border00.png'
 import PortraitBorder01 from '../assets/portrait/border01.png'
+import PortraitBorder02 from '../assets/portrait/border02.png'
 import Portrait01 from '../assets/portrait/portrait01.png'
 import Portrait02 from '../assets/portrait/portrait02.png'
 import Portrait03 from '../assets/portrait/portrait03.png'
 import Portrait04 from '../assets/portrait/portrait04.png'
 import Portrait05 from '../assets/portrait/portrait05.png'
 
-export const portraitBorderImages = [PortraitBorder00, PortraitBorder01]
+export const portraitBorderImages = [
+  PortraitBorder00,
+  PortraitBorder01,
+  PortraitBorder02,
+]
 
 export const portraitImages = [
   Portrait01,
