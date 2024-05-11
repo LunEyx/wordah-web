bc5df0cfbded937c109461b5308aeb3a9e837043
LunEyx <luneyxlab@gmail.com>
2024-05-11 23:02:42 +0800

    Add empty portrait border

--- src/constants/portrait.ts
+++ src/constants/portrait.ts
@@ -1,3 +1,4 @@
+import PortraitBorder00 from '../assets/portrait/border00.png'
 import PortraitBorder01 from '../assets/portrait/border01.png'
 import Portrait01 from '../assets/portrait/portrait01.png'
 import Portrait02 from '../assets/portrait/portrait02.png'
@@ -5,7 +6,7 @@ import Portrait03 from '../assets/portrait/portrait03.png'
 import Portrait04 from '../assets/portrait/portrait04.png'
 import Portrait05 from '../assets/portrait/portrait05.png'
 
-export const portraitBorderImages = [PortraitBorder01]
+export const portraitBorderImages = [PortraitBorder00, PortraitBorder01]
 
 export const portraitImages = [
   Portrait01,
